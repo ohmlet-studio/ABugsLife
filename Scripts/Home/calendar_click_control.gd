7c2af23e9660b7779311b735ff8058bedf703a89
dandelionda <pv8si92z@addy.to>
2025-07-31 17:47:11 +0200

    calendar tick + files

---
+++ Scripts/Home/calendar_click_control.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+@onready var sound_player = get_parent().get_node("AudioStreamPlayer")
+
+func fade_in_children():
+	var tween = create_tween()
+	for child in get_children():
+		var delay: float = randf_range(.2, .7)
+		tween.tween_property(child, "modulate:a", 1.0, delay)
+
+
+func _ready() -> void:
+	for child in get_children():
+		child.modulate.a = 0
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			fade_in_children()
+			sound_player.play()
