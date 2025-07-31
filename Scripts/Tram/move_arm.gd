05dd9309bb93eab8bf4fcc7f3607a960f4d97975
RubenGres <ruben.gres@proton.me>
2025-08-01 00:32:46 +0200

    started keboard and moved files around

--- Scripts/Tram/move_arm.gd
+++ Scripts/Tram/move_arm.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Sprite2D
 
 func _process(delta: float) -> void:
 	global_position = get_global_mouse_position()
--- Scripts/tram_scene.gd
+++ Scripts/tram_scene.gd
@@ -2,5 +2,7 @@ extends Node2D
 
 @onready var interior_animation_player = $TramInterior/AnimationPlayer
 
+signal scene_completed
+
 func _ready():
 	interior_animation_player.play("TramMovement")
